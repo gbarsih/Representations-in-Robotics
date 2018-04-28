81addedb94b1d033362aa4a09f537eaf85d32662
Gabriel Barsi Haberfeld <gbh2@illinois.edu>
2018-04-27 20:35:18 -0500

    Add files via upload

--- coverage/main.m
+++ coverage/main.m
@@ -1,17 +1,30 @@
-% clc; close all;
-% graph = [6, 0, 0, 0, 0, 0];
-% agts = agents(graph);
+%% Initialize
 
-%%% After initialization
+clc; close all;
+graph = [6, 0, 0, 0, 0, 0];
+agts = agents(graph);
+
+%% After initialization
 
 for j=1:length(agts)
-  if agts(j).i == agts(j).k*(agts(j).k - 1)/2
-    agts(j).term = true;
-  end
-  if ~agts(j).term
-    cross(agts, j);
-  end
+    if agts(j).i == agts(j).k*(agts(j).k - 1)/2
+        agts(j).term = true;
+    end
+    if ~agts(j).term
+        cross(agts, j);
+    end
 end
+
+%% Update i's and graph
+
+for j=1:length(agts)
+    if ~agts(j).term
+        agts(j).i = agts(j).i - 1;
+    end
+end
+
 graph = updategraph(graph, agts);
+
+%% Plot
 
 bar(graph)
