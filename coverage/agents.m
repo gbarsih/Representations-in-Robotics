3eddd4cbc8e2be04a270c9575b6869143b06a2d5
Arun Lakshmanan <lakshma2@illinois.edu>
2018-05-04 00:45:49 -0500

    Update matlab coverage simulator

--- coverage/agents.m
+++ coverage/agents.m
@@ -1,7 +1,7 @@
-function objArr = Agent(graph)
+function objArr = agents(graph)
   objArr = [];
-  % N = length(graph);
-  N = graph(1);
+  N = length(graph);
+  % N = graph(1);
   i = N*(N-1)/2;
   for j = 1:graph(1)
     obj = Agent(j,1,false,i,N-1);
--- coverage/main.m
+++ coverage/main.m
@@ -1,15 +1,13 @@
 %% Initialize
 
 clc; close all;
-graph = [9, 0, 0, 0, 0, 0];
+graph = [6, 0, 0, 0, 0, 0];
 agts = agents(graph);
 
 %% Loop
 
 while ~terminated(agts)
-    
-   
-    
+
     for j=1:length(agts)
         if agts(j).i == agts(j).k*(agts(j).k - 1)/2
             agts(j).term = true;
@@ -18,17 +16,14 @@ while ~terminated(agts)
             cross(agts, j);
         end
     end
-    
-   
-    
+
     for j=1:length(agts)
         if ~agts(j).term
             agts(j).i = agts(j).i - 1;
         end
     end
-    
+
     graph = updategraph(graph, agts);
-    
 end
 
 %% Plot
